5b11b9e1055d69f0623f39d5c77178bf4c0b557d
asmitamark <65702735+asmitamark@users.noreply.github.com>
2020-05-21 13:18:06 +0530

    Add files via upload

---
+++ Project/mic.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+r=sr.Recogniser()
+wuth sr.Microphone() as source:
+print("Say something")
+audio=r.listen(source)
+
+try:
+    print("Google speech recognition thinks you said " +r.recognize_google(audio))
+except sr.UnknownValueError :
+    print("Google speech recognition could not understand audio")
+    except sr.RequestError as e:
+    print("Could not request resukts from google speech recognition service; {0}".format(e))
